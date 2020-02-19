f4e31088f525104eaf111976d33f407285c606bc
Javier Gradiche <jg@qubikdigital.com>
2020-02-19 15:51:19 -0300

    Move cask order

--- Cask/kuberails.rb
+++ Cask/kuberails.rb
@@ -1,23 +1,24 @@
 cask 'kuberails' do
   version '0.0.1'
-  sha256 ''
+  sha256 'dc1eca6ae99a9d3c7acd225284a778fd56c95c4ed93395708a1ef19403903083'
 
-  url "https://github.com/javiergradiche/kuberails/releases/download/v#{version}/kuberails.zip"
+  url "https://github.com/javiergradiche/homebrew-kuberails/archive/#{version}.zip"
 
   name 'Kuberails CLI'
   homepage 'https://www.coworkear.com'
 
   depends_on macos: '>= :mojave'
   depends_on formula: "csshx"
+  
+  stage_only
+  
+  preflight do
+    system_command 'bundle install'
+  end
 
-  artifact 'kuberails.rb', target: "#{ENV['HOME']}/bin/kuberails.rb"
-  binary 'kuberails'
-
-  installer script: {
-    executable: "#{staged_path}/bundle",
-    args:       ['install'],
-    sudo:       false,
-  }
+  postflight do
+    system_command 'bundle install'
+  end
 
   zap delete: [
     '~/bin/kuberails.rb',
---
+++ README.md
@@ -0,0 +1,14 @@
+---
+
+# Kuberails CLI Cheat Sheet
+
+## Install the Kuberails CLI
+
+### Using an Apple Machine
+
+Installation on mac uses homebrew. If you do not have homebrew installed see <https://brew.sh/>
+
+```bash
+brew tap javiergradiche/kuberails
+brew cask install javiergradiche/kuberails
+```
